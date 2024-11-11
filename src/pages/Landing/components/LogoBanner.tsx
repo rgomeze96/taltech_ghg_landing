c284871c20b95a66dc3fe3b43cd448d09e72e48f
rgomeze96 <rgomez51496@gmail.com>
2024-11-10 20:46:34 -0500

    eu-logo and google tag to head

--- src/pages/Landing/components/LogoBanner.tsx
+++ src/pages/Landing/components/LogoBanner.tsx
@@ -1,4 +1,3 @@
-import EUCofounding from "@/assets/eu-cofounding.svg?react";
 import Kliimam from "@/assets/kliimaministeerium.svg?react";
 import TaltechPink from "@/assets/taltech-pink.svg?react";
 
@@ -9,7 +8,13 @@ const LogoBanner = () => {
         <div className="w-full max-w-[1440px] mx-auto flex flex-col md:flex-row items-center justify-around px-20 py-10">
           <TaltechPink className="w-[178px] h-[141px]" />
           <Kliimam className="w-[178px] h-[141px]" />
-          <EUCofounding className="w-[178px] h-[141px] pr-7" />
+          <img
+            src="/eu-cofounding.webp"
+            width={200}
+            height={150}
+            alt="TalTech logo"
+            className="cursor-pointer"
+          />
         </div>
       </div>
     </>
